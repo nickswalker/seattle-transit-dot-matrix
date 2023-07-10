4d2b0f24bc6054cf00315b02c115b47951db67d8
Nick Walker <nick@nickwalker.us>
2023-07-09 21:48:35 -0700

    Add initial characters and build pipeline
    Add known characters for Link-related dot matrix displays

---
+++ README.md
@@ -0,0 +1,37 @@
+# Seattle Transit Dot Matrix Typeface
+
+A set of dot matrix fonts in the style of Sound Transit's displays. Inspired by [London TFL Dot Matrix Typeface](https://github.com/petykowski/London-Underground-Dot-Matrix-Typeface).
+
+## Seattle Transit 7
+
+Used for secondary text on station platform displays (Angle Lake to Westlake).
+
+## Seattle Transit 12
+
+Used for Link Light Rail train interior displays.
+
+## Seattle Transit 15
+
+Used on Link Light Rail station platforms (Angle Lake to Westlake). Upper and lower case.
+
+## Seattle Transit 16
+
+Used in Link Light Rail interior displays. Upper case only.
+
+## Seattle Transit Headsign 16
+
+Used in Link Light Rail headsigns. Upper case only.
+
+## Contribute
+
+#### Build Additional Characters to Existing Typeface
+
+
+#### Generate Font
+1. File > Generate Fonts
+   - Maintain default export values
+2. Save into `font` directory of repo 
+
+## License
+
+This typeface is distributed with an [SIL Open Font License](http://scripts.sil.org/OFL).
---
+++ bin2ufo.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from collections import defaultdict
+
+from fontTools.misc.transform import Transform
+from fontTools.pens.transformPen import TransformPen
+
+from fontTools.pens.pointPen import SegmentToPointPen
+from fontTools.ufoLib.glifLib import writeGlyphToString
+from fontTools.ufoLib import UFOWriter
+from fontmake.font_project import FontProject
+
+__version__ = "1.0.0"
+
+DOT_SCALE = 80
+RIGHT_MARGIN = 1
+
+unicode_map = {
+    "colon": ord(':'),
+    "lparen": ord('('),
+    "rparen": ord(')'),
+    "period": ord('.'),
+    "slash": ord('/'),
+    "space": ord(' '),
+    "hairspace": ord(' '),
+    "comma": ord(',')}
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def make_woff2(files, destination):
+  """
+  Makes WOFF2 files from list of paths.
+
+  *files* is a `list` of file paths as `string`
+  *destination* is a `string` of the destination to save the WOFF files.
+  """
+  from fontTools.ttLib import woff2
+  from fontTools.ttx import makeOutputFileName
+
+  if not os.path.exists(destination):
+    os.mkdir(destination)
+
+  for i, file in enumerate(files):
+    outfilename = makeOutputFileName(file,
+                                     outputDir=destination,
+                                     extension='.woff2',
+                                     overWrite=True)
+    if os.path.exists(outfilename):
+      os.remove(outfilename)
+
+    woff2.compress(file, outfilename)
+
+
+def bin2glyph(binary_data, name, width=0, height=0, transform=None,
+             version=2):
+    """ Convert an SVG outline to a UFO glyph, and assign the given 'name',
+    advance 'width' and 'height' (int), 'unicodes' (list of int) to the
+    generated glyph.
+    Return the resulting string in GLIF format (default: version 2).
+    If 'transform' is provided, apply a transformation matrix before the
+    conversion (must be tuple of 6 floats, or a FontTools Transform object).
+    """
+    glyph = DotOutline.fromdata(binary_data, transform=transform)
+    glyph.name = name
+    if len(name) == 1:
+        unicode = ord(name)
+    elif len(name) == 2 and name[1] == "_":
+        unicode = ord(name[0])
+    else:
+        unicode = unicode_map[name]
+    glyph.unicodes = [unicode]
+    return glyph
+
+
+lines_to_data = lambda lines: [list(map(lambda char: True if char == '1' else False, list(line))) for line in lines]
+class DotOutline(object):
+    """ Parse SVG ``path`` elements from a file or string, and draw them
+    onto a glyph object that supports the FontTools Pen protocol, or
+    the ufoLib (ex RoboFab) PointPen protocol.
+
+    For example, using a Defcon Glyph:
+
+        import defcon
+
+        glyph = defcon.Glyph()
+        pen = glyph.getPen()
+        svg = SVGOutline("path/to/a/glyph.svg")
+        svg.draw(pen)
+
+        pen = glyph.getPointPen()
+        svg = SVGOutline.fromstring('<?xml version="1.0" ...')
+        svg.drawPoints(pen)
+
+    The constructor can optionally take a 'transform' matrix (6-float tuple,
+    or FontTools Transform object).
+    """
+
+    def __init__(self, filename=None, transform=None):
+        if filename:
+            with open(filename) as file_in:
+                lines = file_in.readlines()
+
+            self.data = lines_to_data(lines)
+        else:
+            self.data = []
+        self.transform = transform
+
+    @classmethod
+    def fromstring(cls, data, transform=None):
+        self = cls(transform=transform)
+        self.data = lines_to_data(data)
+        return self
+
+    @classmethod
+    def fromdata(cls, data, transform=None):
+        self = cls(transform=transform)
+        self.data = data
+        return self
+
+
+    @property
+    def width(self):
+        return (len(self.data[0]) + RIGHT_MARGIN) * DOT_SCALE
+
+    @property
+    def height(self):
+        return (len(self.data) + RIGHT_MARGIN) * DOT_SCALE
+
+    def draw(self, pen):
+        d = .65
+        if self.transform:
+            pen = TransformPen(pen, self.transform)
+        pen = TransformPen(pen, Transform(dx=DOT_SCALE * d/2, dy=DOT_SCALE * d/2).scale(DOT_SCALE, DOT_SCALE))
+
+        for j, line in enumerate(reversed(self.data)):
+            for i, dot in enumerate(line):
+                if not dot:
+                    continue
+                size_x = size_y = d / 2
+                pen.moveTo((i + size_x, j))
+                pen.curveTo(
+                  (i + size_x, j - (0.552 * size_y)),
+                  (i + (0.552 * size_x), j - size_y),
+                  (i - 0, j - size_y))
+                pen.curveTo(
+                  (i - (0.552 * size_x), j - size_y),
+                  (i - size_x, j - (0.552 * size_y)),
+                  (i - size_x, j - 0))
+
+                pen.curveTo(
+                  (i - size_x, j + (0.552 * size_y)),
+                  (i - (0.552 * size_x), j + size_y),
+                  (i - 0, j + size_y))
+
+                pen.curveTo(
+                  (i + (0.552 * size_x), j + size_y),
+                  (i + size_x, j + (0.552 * size_y)),
+                  (i + size_x, j))
+
+                pen.closePath()
+
+    def drawPoints(self, pointPen):
+        pen = SegmentToPointPen(pointPen)
+        self.draw(pen)
+
+
+def parse_args(args):
+    import argparse
+
+    def split(arg):
+        return arg.replace(",", " ").split()
+
+    def unicode_hex_list(arg):
+        try:
+            return [int(unihex, 16) for unihex in split(arg)]
+        except ValueError:
+            msg = "Invalid unicode hexadecimal value: %r" % arg
+            raise argparse.ArgumentTypeError(msg)
+
+    def transform_list(arg):
+        try:
+            return [float(n) for n in split(arg)]
+        except ValueError:
+            msg = "Invalid transformation matrix: %r" % arg
+            raise argparse.ArgumentTypeError(msg)
+
+    parser = argparse.ArgumentParser(
+        description="Convert SVG outlines to UFO glyphs (.glif)")
+    parser.add_argument(
+        "outfile", metavar="out", help="Output directory")
+
+    parser.add_argument(
+        "-f", "--format", help="UFO GLIF format version (default: 2)",
+        type=int, choices=(1, 2), default=2)
+    parser.add_argument('--version', action='version', version=__version__)
+
+    return parser.parse_args(args)
+
+
+def load_from_txt(path):
+    characters = {}
+    for filename in os.listdir(path):
+        if not filename.endswith(".txt"):
+            continue
+        with open(f"{path}/{filename}") as fp:
+            lines = fp.readlines()
+        data = lines_to_data(map(str.strip, lines))
+        if len(data) == 0:
+            continue
+        characters[filename.split(".")[0]] = data
+    return characters
+
+def create_ufo(name, path, character_data, format, info):
+    writer = UFOWriter(f"{path}/{name}.ufo")
+    glyphset = writer.getGlyphSet()
+
+    for character, data in character_data.items():
+        glif = bin2glyph(data, character,
+                         version=format)
+        glyphset.writeGlyph(character, glif, glif.drawPoints, format)
+
+    glyphset.writeContents()
+    writer.writeLayerContents()
+    writer.writeInfo(info)
+    return writer
+
+def make_attr_dict(family_name, dot_height, x_dot_height, attrs={}):
+    ascender_height = DOT_SCALE * dot_height
+    descender_height = -DOT_SCALE
+    # See https://googlefonts.github.io/gf-guide/metrics.html
+    return AttrDict({"familyName": f"{family_name} {dot_height}",
+        "versionMajor": 1,
+        "versionMinor": 0,
+        "copyright": "Copyright (c) 2023, Nick Walker",
+        "openTypeNameVersion": "Version 1.000",
+        "openTypeOS2Selection": [7], # Set "Use Typo Metrics"
+        "openTypeOS2WeightClass": 5,  # Normal width
+        "openTypeOS2WeightClass": 700,  # Bold
+        "openTypeOS2TypoAscender": ascender_height,
+        "openTypeOS2TypoDescender": descender_height,
+        "openTypeOS2TypoLineGap": 0,
+        "openTypeOS2WinAscent": ascender_height,
+        "openTypeOS2WinDescent": -descender_height,
+        "openTypeHheaAscender": ascender_height,
+        "openTypeHheaDescender": descender_height,
+        "openTypeHheaLineGap": 0,
+        "note": "Created with bin2ufo.py and fonttools",
+        "ascender": ascender_height,
+        "descender": descender_height,
+        "unitsPerEm": DOT_SCALE * dot_height, # Sum of metrics is expected to be 120-130% this value
+        "xHeight": DOT_SCALE * x_dot_height,
+        "italicAngle": 0})
+
+def main(args=None):
+    options = parse_args(args)
+    st_7_info = make_attr_dict("Seattle Transit", 7, 5)
+    sound_transit_7 = load_from_txt("sound_transit_7")
+    st_7_ufo = create_ufo("sound_transit_7", "out", sound_transit_7, options.format, st_7_info)
+
+    st_12_info = make_attr_dict("Seattle Transit", 12, 8)
+    sound_transit_12 = load_from_txt("sound_transit_12")
+    st_12_ufo = create_ufo("sound_transit_12", "out", sound_transit_12, options.format, st_12_info)
+    st_15_info = make_attr_dict("Seattle Transit", 15, 11)
+    sound_transit_15 = load_from_txt("sound_transit_15")
+    st_15_ufo = create_ufo("sound_transit_15", "out", sound_transit_15, options.format, st_15_info)
+    st_16_info = make_attr_dict("Seattle Transit", 16, 11)
+    sound_transit_16 = load_from_txt("sound_transit_16")
+    st_16_ufo = create_ufo("sound_transit_16", "out", sound_transit_16, options.format, st_16_info)
+
+    FontProject().run_from_ufos("out/*.ufo", ("otf", "ttf"), output_dir="out")
+    make_woff2(["out/sound_transit_7.otf"], "out")
+    make_woff2(["out/sound_transit_15.otf"], "out")
+
+
+if __name__ == "__main__":
+    main()
